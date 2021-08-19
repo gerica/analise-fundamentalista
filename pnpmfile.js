6d01e26ea10b0229ac90cff55e7c848c7069f753
gerica <rogeriogerica@gmail.com>
2021-08-19 10:13:47 -0300

    resolver problema do pino-petty

---
+++ pnpmfile.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-param-reassign */
+module.exports = {
+  hooks: {
+    readPackage(pkg) {
+      if (pkg.name === 'pino') {
+        pkg.peerDependencies = pkg.peerDependencies || {};
+        pkg.peerDependencies['pino-pretty'] = '*';
+      }
+      return pkg;
+    },
+  },
+};
